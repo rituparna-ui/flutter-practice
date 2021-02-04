1ee228a1de5351fa4d65c7d22e5553bac4370576
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-02-04 21:39:08 +0530

    I am rich app, no icon

---
+++ i_am_rich/android/app/src/main/kotlin/com/rituparnawarwatkar/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rituparnawarwatkar.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.teal,
+        body: Center(
+          child: Image(
+            image: AssetImage(
+              'src/images/dua.png',
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
