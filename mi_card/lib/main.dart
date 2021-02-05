9472e94ececcf4734b2f1d240bec32154f041a7b
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-02-06 01:23:57 +0530

    Complete MiCard

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -10,11 +10,12 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
                 backgroundColor: Colors.red,
-                backgroundImage: AssetImage('img/me.jpg'),
+                backgroundImage: AssetImage('src/img/me.jpg'),
               ),
               Text(
                 'Rituparna W',
@@ -22,8 +23,71 @@ class MyApp extends StatelessWidget {
                   fontSize: 40,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
                 ),
-              )
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceCodePro',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.black,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                    size: 40,
+                  ),
+                  title: Text(
+                    '+91 77985 1664',
+                    style: TextStyle(
+                      fontFamily: 'SourceCodePro',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                    size: 40,
+                  ),
+                  title: Text(
+                    'rwarwatkar@gmail.com',
+                    style: TextStyle(
+                        fontFamily: 'SourceCodePro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.teal.shade900),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
